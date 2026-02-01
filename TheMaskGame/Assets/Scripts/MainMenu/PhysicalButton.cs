fa35b43b9358f7c8ed1edc02375acb600d670093
Nazar Protasov <nzrprotasov@gmail.com>
2026-02-01 12:31:48 +0200

    Main menu 3D created

---
+++ TheMaskGame/Assets/Scripts/MainMenu/PhysicalButton.cs
@@ -0,0 +1,35 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class PhysicalButton : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onClick;
+    [SerializeField] private float animationDuration;
+    [SerializeField] private Transform movable;
+    [SerializeField] private Vector3 targetLocalPos;
+    private bool isAnimating;
+
+    private void OnMouseUpAsButton()
+    {
+        if (isAnimating)
+        {
+            return;
+        }
+        isAnimating = true;
+        movable.DOLocalMove(targetLocalPos, animationDuration / 2).OnComplete(OnComplete);
+    }
+
+    private void OnComplete()
+    {
+        onClick.Invoke();
+        movable.DOLocalMove(Vector3.zero, animationDuration / 2).OnComplete(End);
+    }
+
+    private void End()
+    {
+        isAnimating = false;
+    }
+
+}
